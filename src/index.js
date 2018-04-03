c33a0871c2b4c1528d23efe4d5f35a5389d1d812
zhaolandelong <zhaolandelong@163.com>
2018-04-03 15:23:08 +0800

    webpack init
    Change-Id: Id8e1e461339d668a25e9bc42010c7c4fb3389717

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import { strReverse } from '@util';
+import './index.less';
+import logo from './assets/logo.jpg';
+
+const $app = document.getElementById('app');
+
+const strInput = 'Hello World';
+const srtHolder = 'The result will be here...';
+const strHtml = `  
+<img src="${logo}" alt="logo" />    
+<h1>${strInput}</h1>    
+<button id="do">Show the reverse of "${strInput}"</button>  
+<button id="reset">Do reset</button>  
+<p id="result">${srtHolder}</p>  
+`;
+
+$app.innerHTML = strHtml;
+
+const $result = document.getElementById('result');
+
+document.getElementById('do').onclick = () => {
+  $result.innerHTML = `The reverse of "${strInput}" is "${strReverse(strInput)}"`;
+};
+document.getElementById('reset').onclick = () => {
+  $result.innerHTML = srtHolder;
+};
---
+++ src/util/index.js
@@ -0,0 +1,7 @@
+export function strReverse(str) {
+  return str.split('').reverse().join('');
+}
+
+export function add(a, b) {
+  return a + b;
+}
